632742559486707af05ea85d0aba04b608cce6ad
JuanElKantar <128154436+JuanElKantar@users.noreply.github.com>
2023-11-22 03:03:00 -1200

    Update register.component.ts

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 interface Credencial {
   tipoID: string;
@@ -14,13 +15,14 @@ interface Credencial {
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-   public credenciales: Credencial[] = [];
-  constructor(public router: Router){
-    this.insertData()
+  public credenciales: Credencial[] = [];
+
+  constructor(public router: Router) {
+    this.insertData();
   }
-  insertData(){
+
+  insertData() {
     this.credenciales = [
-     
       {
         tipoID: "CC",
         ID: "3131",
@@ -29,26 +31,49 @@ export class RegisterComponent {
       }
     ];
 
-    localStorage.setItem('data', JSON.stringify(this.credenciales))
+    localStorage.setItem('data', JSON.stringify(this.credenciales));
   }
 
-  createUser(){
-    const tipoID = (document.getElementById("tipoID") as HTMLSelectElement).value
-    const ID = (document.getElementById("ID") as HTMLInputElement).value
-    const nombre = (document.getElementById("name") as HTMLInputElement).value
-    const password = (document.getElementById("pass") as HTMLInputElement).value
-    console.log(tipoID,ID,nombre,password)
+  createUser() {
+    const tipoID = (document.getElementById("tipoID") as HTMLSelectElement).value;
+    const ID = (document.getElementById("ID") as HTMLInputElement).value;
+    const nombre = (document.getElementById("name") as HTMLInputElement).value;
+    const password = (document.getElementById("pass") as HTMLInputElement).value;
 
-    const nuevasCredenciales={
-      tipoID:tipoID,
-      ID:ID,
-      nombre:nombre, 
-      contraseña:password
+    // Validaciones adicionales
+    if (ID.length < 6) {
+      Swal.fire('Error', 'Número de documento debe tener al menos 6 caracteres', 'error');
+      return;
     }
-    this.credenciales.push(nuevasCredenciales)
+
+    if (nombre.length < 2) {
+      Swal.fire('Error', 'Nombre debe tener más de 1 caracter', 'error');
+      return;
+    }
+
+    if (password.length < 8 || !/\d/.test(password)) {
+      Swal.fire('Error', 'Contraseña debe tener al menos 8 caracteres y contener al menos un número', 'error');
+      return;
+    }
+
+    const nuevasCredenciales = {
+      tipoID: tipoID,
+      ID: ID,
+      nombre: nombre,
+      contraseña: password
+    };
+
+    this.credenciales.push(nuevasCredenciales);
 
     localStorage.setItem('data', JSON.stringify(this.credenciales));
-    this.clearForm()
+    this.clearForm();
+    Swal.fire({
+      title: 'Éxito',
+      text: 'Usuario registrado exitosamente',
+      icon: 'success'
+    }).then(() => {
+      this.router.navigateByUrl('/');
+    });
   }
 
   clearForm() {
@@ -58,7 +83,7 @@ export class RegisterComponent {
     (document.getElementById("pass") as HTMLInputElement).value = "";
   }
 
-  navegarLogin(){
-    this.router.navigateByUrl('/')
+  navegarLogin() {
+    this.router.navigateByUrl('/');
   }
 }
