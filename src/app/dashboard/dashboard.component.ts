12f613ea0be272421ca7583d3cf3eac63549ae07
JuanElKantar <128154436+JuanElKantar@users.noreply.github.com>
2023-11-21 23:05:08 -0500

    Update dashboard.component.ts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,12 +6,16 @@ import { Router } from '@angular/router';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+
 export class DashboardComponent {
   // Lista de rutas
   rutas: string[] = ['Ruta 1', 'Ruta 2', 'Ruta 3'];
 
   // Variable para almacenar el nombre de la nueva ruta
   nuevaRuta: string = '';
+
+  // Variable para controlar qué formulario se muestra
+  formularioVisible: 'crearRuta' | 'anadirConductor' | 'estacionNueva' | null = null;
 
   constructor(public router: Router) { }
 
@@ -25,5 +29,10 @@ export class DashboardComponent {
       this.rutas.push(this.nuevaRuta);
       this.nuevaRuta = ''; // Limpiar el campo después de agregar la ruta
     }
+  }
+
+  // Método para mostrar el formulario correspondiente
+  mostrarFormulario(formulario: 'crearRuta' | 'anadirConductor' | 'estacionNueva') {
+    this.formularioVisible = formulario;
   }
 }
